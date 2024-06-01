8e5a22c2331af81e42d0ee7f7bf73254f211ecad
jkasprowicz <joao.kasprowicz@univali.br>
2024-05-31 22:27:26 -0300

    extract_pdf

--- blood_test_tracker/tracker/models.py
+++ blood_test_tracker/tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ExamResult(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    data_entrada = models.DateField()
+    material = models.CharField(max_length=100)
+    exam_type = models.CharField(max_length=100)
+    results = models.TextField()
+    method = models.CharField(max_length=100)
+    reference_value = models.CharField(max_length=100)
+    note = models.TextField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
