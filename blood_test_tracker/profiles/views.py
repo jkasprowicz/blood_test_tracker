f0a770e734a895ddfabb7a1666fd1db17b9b8861
jkasprowicz <joao.kasprowicz@univali.br>
2024-07-12 22:18:59 -0300

    working up with the system

--- blood_test_tracker/profiles/views.py
+++ blood_test_tracker/profiles/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate
 from .models import UserProfile
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
+from tracker.models import ExamResult
 
 def dashboard(request):
     user_groups = request.user.groups.all()
@@ -16,4 +17,9 @@ def dashboard(request):
 
 @login_required
 def group_home(request, group_name):
-    return render(request, 'group_home.html')
+    user = request.user
+    latest_exams = ExamResult.objects.filter(name=user.username).order_by('-uploaded_at')[:5]
+    return render(request, 'group_home.html', {
+        'user': user,
+        'latest_exams': latest_exams
+    })
